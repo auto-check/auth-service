1a2217ee4b5bc009386b0bb7c7712d531a097478
hwc9169 <hwc9169@gmail.com>
2021-06-01 14:12:36 +0900

    Add LoginAuth handler

--- handler/student_handler.go
+++ handler/student_handler.go
@@ -1,25 +1,55 @@
-package controller
+package handler
 
 import (
+	"auth/model"
+	"auth/module/client"
 	authpb "auth/protocol-buffer/golang/auth"
 	"auth/usecase"
-	"github.com/labstack/echo"
+	"context"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
-type StudentController struct {
+type StudentHandler struct {
 	authpb.AuthServer
-	usecase.StudentUsecase
+	su usecase.StudentUsecase
 }
 
-func NewStudentController(e *echo.Echo, us usecase.StudentUsecase) {
-	handler := &StudentController{
-		StudentUsecase: us,
+func NewStudentHandler(gserver *grpc.Server, us usecase.StudentUsecase) {
+	handler := &StudentHandler{
+		su: us,
+	}
+	authpb.RegisterAuthServer(gserver, handler)
+}
+
+func (sh *StudentHandler) LoginAuth(ctx context.Context, r *authpb.LoginAuthRequest) (*authpb.LoginAuthResponse, error) {
+	oauthToken, err := client.GetOauthAccessToken(r.AuthorizationCode)
+	if err != nil {
+		return nil, err
 	}
 
-	e.POST("/login", handler.Login)
+	s, err := client.GetOauthInfo(oauthToken)
+	if err != nil {
+		return nil, err
+	}
+
+	at, rt, err := sh.su.LoginAuth(&model.Student{
+		Name: s.Name,
+		Gcn: s.Gcn,
+		Email: s.Email,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info(at)
+
+	return &authpb.LoginAuthResponse{AccessToken: at, RefreshToken: rt}, nil
 }
 
-func (s *StudentController) Login(c echo.Context) error {
 
-	return nil
+func (sh *StudentHandler) GetStudentWithId(ctx context.Context, req *authpb.GetStudentWithIdRequest) (*authpb.GetStudentWithIdResponse, error) {
+
+	return nil, nil
 }
+
