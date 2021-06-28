357ac76607a9f8585e18658ff82600d544cd602e
hwc9169 <hwc9169@gmail.com>
2021-06-28 22:08:39 +0900

    test for main client

--- handler/student_handler_test.go
+++ handler/student_handler_test.go
@@ -1,17 +1,21 @@
 package handler
 
 import (
-	authpb "auth/protocol-buffer/golang/auth"
 	"auth/usecase/mocks"
 	"bytes"
 	"context"
 	"encoding/json"
 	"github.com/auto-check/common-module/jwt"
 	"github.com/auto-check/common-module/model"
+	mcmocks "github.com/auto-check/main-service/client/mocks"
+	authpb "github.com/auto-check/protocol-buffer/golang/auth"
+	"github.com/golang/mock/gomock"
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/emptypb"
 	"net/http"
 	"os"
 	"strings"
@@ -62,14 +66,24 @@ func TestLoginAuth(t *testing.T) {
 	err = json.NewDecoder(resp.Body).Decode(&respBody)
 	code := strings.Split(respBody.Location, "=")[1]
 
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	/* handler mock 객체 생성 */
+	mcMock := mcmocks.NewMockMainClient(ctrl)
+	ctx := metadata.NewIncomingContext(context.Background(), metadata.MD{"student_id": []string{"1"}})
+	mcMock.EXPECT().CreateMacro(
+		ctx,
+		&emptypb.Empty{},
+	).Return(&emptypb.Empty{}, nil)
+	sh := NewStudentHandler(grpc.NewServer(), mockStudent, mcMock)
+
 	/* authorization code로 LoginAuth 테스트*/
-	sh := NewStudentHandler(grpc.NewServer(), mockStudent)
-	loginAuthResponse, err := sh.LoginAuth(context.Background(), &authpb.LoginAuthRequest{
+	loginAuthResponse, err := sh.LoginAuth(ctx, &authpb.LoginAuthRequest{
 		Code: code,
 	})
 
 	studentID,  err := jwt.ParseStudentIDFromToken(loginAuthResponse.AccessToken)
-
 
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), studentID)
